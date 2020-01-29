b9506e67b5538fbd8f546f21b97eb4ba592f2d5c
farazalam030 <farazalam030@gmail.com>
2020-01-29 10:36:31 +0530

    Add AnagramGrouping via Maps

---
+++ anaGramGroupSort.cpp
@@ -0,0 +1,70 @@
+/*
+Given a sequence of words, print all anagrams together | Set 1
+Given an array of words, print all anagrams together. For example, if the given
+array is {"cat", "dog", "tac", "god", "act"}, then output may be "cat tac act
+dog god".
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void groupByAnaGrams(vector<string> &s) {
+  int sSize = s.size();
+  unordered_map<uint64_t, vector<string>> mmap;
+  uint64_t mask = 0;
+  uint64_t key = 0;
+  int len = 0;
+  for (auto i : s) {
+    mask = 0;
+    len = i.length();
+
+    for (int j = 0; j < len; j++) {
+
+      if (i[j] >= 'a' && i[j] <= 'z') {
+        mask = mask | (0x1 << (i[j] - 'a'));
+        continue;
+      }
+      if (i[j] >= 'A' && i[j] <= 'Z') {
+        mask = mask | (0x1 << (i[j] - 'A' + 26));
+        continue;
+      }
+    }
+    key = mask + len;
+    if (mmap.find(key) == mmap.end()) {
+      mmap[key].push_back(i);
+    } else {
+      mmap[key].push_back(i);
+    }
+  }
+  for (auto i : mmap) {
+    cout << hex << i.first << " -> ";
+    for (auto j : i.second) {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+  cout << endl;
+}
+
+int main(int argc, char const *argv[]) {
+
+  //{"cat", "dog", "tac", "god", "act"}
+  int n;
+  cin >> n;
+  vector<string> s(n, ""); //{"cat", "dog", "tacc", "god", "act", "c",   "cc",
+                           //"ccccccccccccccc"};
+  for (int i = 0; i < n; i++) {
+    cin >> s[i];
+  }
+
+  groupByAnaGrams(s);
+
+  //   unordered_map<int, vector<string>> mmap;// key -> list of similar strings
+  /***
+   * key = Mask + length(eachString)
+   * Mask(64 bit long) represents 0-55 bits maps to alphabets a-zA-Z
+   */
+
+  cout << endl;
+  return 0;
+}
