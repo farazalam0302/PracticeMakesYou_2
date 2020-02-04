c4fd9c6fcd1879d7754e993ae613604718a5b0c9
farazalam030 <farazalam030@gmail.com>
2020-02-04 17:19:54 +0530

    header file update

--- linkedListProblems/linkedList_utility.h
+++ linkedListProblems/linkedList_utility.h
@@ -1,5 +1,16 @@
 /*
 linked List utility functions??
+
+   --> void createLinkedList(Node **head_ref) creates a linked list of first
+nine numbers.
+
+   --> void displayList(Node* head) display linked list
+
+   --> void insertNodeatLast(Node **head, int x) insert node at the end of list.
+
+   --> void insertNodeatHead(Node **head, int x) insert node at the head of
+list.
+
 */
 #include <bits/stdc++.h>
 using namespace std;
