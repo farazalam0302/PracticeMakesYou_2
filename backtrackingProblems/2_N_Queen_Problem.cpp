2ec1daea66ea4ddfb77505d35112c7a2b77b25e3
farazalam030 <farazalam030@gmail.com>
2020-08-09 15:57:58 +0530

    add HAmiltonianCycleBAcktracking modify NQueenProblem' '

--- backtrackingProblems/2_N_Queen_Problem.cpp
+++ backtrackingProblems/2_N_Queen_Problem.cpp
@@ -1,4 +1,14 @@
+/*
+ * The n-queens puzzle is the problem of placing n queens on an n×n chessboard
+ * such that no two queens attack each other. Given an integer n, print all
+ * distinct solutions to the n-queens puzzle. Each solution contains distinct
+ * board configurations of the n-queens’ placement, where the solutions are a
+ * permutation of [1,2,3..n] in increasing order, here the number in the ith
+ * place denotes that the ith-column queen is placed in the row with that
+ * number. For eg below figure represents a chessboard [3 1 4 2].
+ */
 #if 0
+
 /* C/C++ program to solve N Queen Problem using
 backtracking */
 #define N 4
