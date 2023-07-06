899e79314982b75db952b6c72083cc7706e132a2
FARAZ ALAM <9081968+farazalam030@users.noreply.github.com>
2023-07-06 09:13:32 +0000

    modify to simpler solution

--- openText/Q6.cpp
+++ openText/Q6.cpp
@@ -6,8 +6,7 @@ struct myPair {
   int frequency;
 };
 
-int main(int argc, char **argv) {
-  string a = "bbaaabbaaabba";
+int maxSwaps(string &a) {
   int n = a.size();
   int cA = 0;
   int cB = 0;
@@ -36,7 +35,46 @@ int main(int argc, char **argv) {
     swaps = swaps + i.second / 3;
   }
 
-  cout << "Swaps = " << swaps << endl;
+  return swaps;
+}
+
+int maxSwaps2(string &a) {
+  int n = a.size();
+  cout << "a.size = " << n << endl;
+  int cA = 0;
+  int cB = 0;
+  int swaps = 0;
+  // vector<pair<char, int>> pairs;
+  for (int i = 0; i < (n); ++i) {
+    if (a[i] == 'a') {
+      cA = 1;
+      while (a[i] == a[i + 1] && a[i] != '\0') {
+        cA++;
+        i++;
+      }
+      // pairs.push_back({'a', cA});
+      swaps += (cA / 3);
+    }
+    if (a[i] == 'b') {
+      cB = 1;
+      while (a[i] == a[i + 1] && a[i] != '\0') {
+        cB++;
+        i++;
+      }
+      // pairs.push_back({'b', cB});
+      swaps += (cB / 3);
+    }
+  }
+  return swaps;
+}
+
+int main(int argc, char **argv) {
+  string a = "abbabbabbabbaaabbabbbabbbabb";
+  int swaps1 = maxSwaps(a);
+  cout << "swaps1 = " << swaps1 << endl;
+
+  swaps1 = maxSwaps2(a);
+  cout << "swaps2 = " << swaps1 << endl;
 
   return 0;
 }
