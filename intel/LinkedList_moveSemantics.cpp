80a31f31dc0ea7c1adcec0054eb735fb92ebea40
FARAZ ALAM <9081968+farazalam030@users.noreply.github.com>
2023-08-14 12:46:31 +0000

    add new Move semantics LL

---
+++ intel/LinkedList_moveSemantics.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class LinkedList
+{
+public:
+    struct Node{
+        int data;
+        Node* next;
+        Node() : data(0) , next(nullptr){}
+        Node(int x) : data(x) , next(nullptr){}
+        Node(int x, Node* n) : data(x) , next(n){}
+    };
+    Node * head ; 
+
+
+    LinkedList(const LinkedList& c){
+        Node * curr = c.head;
+        Node* n = head ;
+        while(curr)
+        {
+            n=new Node(curr->data,curr->next);
+            
+        }
+    }
+
+    // Move Constructor    
+    LinkedList(LinkedList&& c) noexcept : head{nullptr} {
+        swap(head,c.head);
+    }
+
+    LinkedList& operator= (LinkedList&& c){
+        if (this==&c){
+            return *this;
+        }
+        swap(head,c.head);
+        return *this;
+    }
+    ~LinkedList(){
+        cout << "\nLL Destructor called!!\n";
+    }
+};
+
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
--- intel/intelQ1.cpp
+++ intel/intelQ1.cpp
@@ -5,17 +5,27 @@ class Node {
 public:
   int data;
   Node *next;
-  Node() : data(0), next(nullptr) {}
-  Node(int x) : data(x), next(nullptr) {}
-  //  Node(const Node &cp) : data(cp.data) {
-  //
-  //    if (cp.next) {
-  //      next = new Node(*(cp.next));
-  //    }
-  //    cout << "\nCopy Constructor\n";
-  //  }
-  Node(Node &&cp) : data(cp.data), next(cp.next) {
-    cout << "\nIn Move Constructor\n" << endl;
+  Node() : data(0), next(nullptr) {
+    cout << "\nDefault Constructor for data = "<< data << "\n";
+  }
+  Node(int x) : data(x), next(nullptr) {
+    cout << "\nParems Constructor for data = "<< data << "\n";
+  }
+   Node(const Node &cp) : data(cp.data) {
+  
+     if (cp.next) {
+       next = new Node(*(cp.next));
+     }
+     cout << "\nCopy Constructor for data = "<< data << "\n";
+   }
+  Node(Node &&cp)  {
+    if (&cp != this)
+    {
+      data= cp.data;
+      next=cp.next;
+      
+    }
+    cout << "\nIn Move Constructor for data = "<< data << "\n";
   }
 };
 
@@ -35,12 +45,12 @@ public:
     return;
   }
 
-  Node *copyLL(Node *head) {
+  Node copyLL(Node *head) {
     if (head == nullptr) {
       cout << "\nNo list to copy\n";
-      return nullptr;
+      return NULL;
     }
-    Node *retNode = new Node(*head);
+    Node retNode (*head);
     return retNode;
   }
 
@@ -50,7 +60,14 @@ public:
       cout << curr->data << " -> ";
       curr = curr->next;
     }
+
     cout << endl;
+    curr = head;
+    while (curr) {
+      cout << hex << curr << " -> ";
+      curr = curr->next;
+    }
+    cout <<dec<<endl;
   }
 };
 
@@ -62,9 +79,9 @@ int main(int argc, char const *argv[]) {
   myLL.printLL(myLL.Head);
 
   LinkedList dupLL;
-  Node *ret = dupLL.copyLL(myLL.Head);
+  Node ret = dupLL.copyLL(myLL.Head);
 
-  dupLL.printLL(ret);
+  dupLL.printLL(&ret);
 
   return 0;
 }
