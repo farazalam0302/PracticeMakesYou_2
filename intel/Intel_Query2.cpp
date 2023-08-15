3e49688fa0cf631068f8081d4ad20b89b67de643
ALAM, FARAZ <farazalam030@gmail.com>
2023-08-15 10:28:04 +0530

    beautify edit only

--- intel/Intel_Query2.cpp
+++ intel/Intel_Query2.cpp
@@ -1,67 +1,48 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 /*
 given a string of {{}{}{}}******
-
-check balanced parens 
-
+check balanced parens
 */
-bool checkBalance(string & a){
-    int len =  a.length();
-    stack<int> openStack;
-    stack<int> starStack;
-    for (int i = 0; i < len; i++)
-    {
-        if (a[i] == '{')openStack.push(i);
-        else if (a[i] == '*') {
-            starStack.push(i);
+bool checkBalance(string &a) {
+  int len = a.length();
+  stack<int> openStack;
+  stack<int> starStack;
+  for (int i = 0; i < len; i++) {
+    if (a[i] == '{')
+      openStack.push(i);
+    else if (a[i] == '*') {
+      starStack.push(i);
+    } else if (a[i] == '}') {
+      if (!openStack.empty()) {
+        openStack.pop();
+      } else {
+        if (!starStack.empty()) {
+          starStack.pop();
+        } else {
+          return false;
         }
-        else if (a[i] == '}')
-        {
-            if (!openStack.empty()) {
-                openStack.pop();
-            }
-            else{
-                if (!starStack.empty()){
-                     starStack.pop();
-                }
-                else {
-                    return false;
-                }
-            }
-        }
-
+      }
     }
-
-   while (!openStack.empty()) {
-    if (!starStack.empty()){
-        if (openStack.top() < starStack.top())
-        {
-            openStack.pop();
-            starStack.pop();
-        }
-        else 
+  }
+  while (!openStack.empty()) {
+    if (!starStack.empty()) {
+      if (openStack.top() < starStack.top()) {
+        openStack.pop();
+        starStack.pop();
+      } else
         return false;
-        
-    }
-    else 
-        return false;
-   }
-
-
-
-    if (starStack.size() & 0x1) return false;
-    
-    return true;
-    
-
-
+    } else
+      return false;
+  }
+  if (starStack.size() & 0x1)
+    return false;
+  return true;
 }
-int main(int argc, char const *argv[])
-{
-    string inp = "***{{{}}";
-    bool Decision = checkBalance(inp);
-    Decision?cout << "\n\nYes Balanced!!!\n\n" : cout << "\n\nNo Balanced!!!\n\n" ; 
-    
-    return 0;
+int main(int argc, char const *argv[]) {
+  string inp = "***{{{}}";
+  bool Decision = checkBalance(inp);
+  Decision ? cout << "\n\nYes Balanced!!!\n\n"
+           : cout << "\n\nNo Balanced!!!\n\n";
+  return 0;
 }
