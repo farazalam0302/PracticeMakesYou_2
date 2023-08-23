e68ca86bde1f8611de025811109823c9675f38e2
FARAZ ALAM <9081968+farazalam030@users.noreply.github.com>
2023-08-23 11:57:34 +0000

    Dependency Correction

--- DesignPatternsInCPP/SOLID/DependencyInversion.cpp
+++ DesignPatternsInCPP/SOLID/DependencyInversion.cpp
@@ -6,24 +6,43 @@ struct Person {
   string name;
 };
 
-struct Relationships // LL
+struct RelationshipBrowser {
+  virtual vector<Person> findAllChildrenOf(const string &name) = 0;
+};
+
+struct Relationships : RelationshipBrowser // LL
 {
   vector<tuple<Person, Relationship, Person>> relations;
   void addParentAndChild(const Person &parent, const Person &child) {
     relations.push_back({parent, Relationship::parent, child});
     relations.push_back({child, Relationship::child, parent});
   }
+  virtual vector<Person> findAllChildrenOf(const string &name) override {
+    vector<Person> result;
+    for (auto &&[first, rel, second] : relations) {
+      if (first.name == name && rel == Relationship::parent) {
+        result.push_back(second);
+      }
+    }
+    return result;
+  }
 };
 struct Research // HLL
 {
-  Research(Relationships &relationships) {
-    auto &relations = relationships.relations;
-    for (auto &&[first, rel, second] : relations) {
-      if (first.name == "John" && rel == Relationship::parent)
-        cout << "John has a child Called " << second.name << endl;
+  Research(RelationshipBrowser &browser) {
+    for (auto &child : browser.findAllChildrenOf("John")) {
+      cout << "John has a child Called " << second.name << endl;
     }
   }
+  // Research(Relationships &relationships) {
+  //   auto &relations = relationships.relations;
+  //   for (auto &&[first, rel, second] : relations) {
+  //     if (first.name == "John" && rel == Relationship::parent)
+  //       cout << "John has a child Called " << second.name << endl;
+  //   }
+  // }
 };
+
 int main(int argc, char const *argv[]) {
   Person parent{"John"};
   Person child1{"Chris"}, child2{"Matt"};
