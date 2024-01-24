3be8cdcc3d49aa62315400d1c7e14500a823c526
ALAM, FARAZ <farazalam030@gmail.com>
2024-01-24 13:36:00 +0530

    add divide 1n2group query

--- Siemens_devops_AUG_2023/R2_Q1.cpp
+++ Siemens_devops_AUG_2023/R2_Q1.cpp
@@ -11,18 +11,48 @@ Output : 2
          3
          4 3 1
 
+
+https://www.geeksforgeeks.org/divide-1-n-two-groups-minimum-sum-difference/
+
+
+
 */
 #include <bits/stdc++.h>
 using namespace std;
 
-void solution(int n) { vector }
+void printArray(vector<int> &a) {
+  cout << endl;
+  for (auto i : a) {
+    cout << i << ", ";
+  }
+  cout << endl;
+}
+
+void solution(int n) {
+  vector<int> g1, g2;
+
+  int g1Sum = n * (n + 1) / 4;
+  for (int i = n; i >= 1; --i) {
+
+    if (g1Sum - i >= 0) {
+      g1.push_back(i);
+      g1Sum -= i;
+    } else {
+      g2.push_back(i);
+    }
+  }
+
+  printArray(g1);
+
+  printArray(g2);
+}
 
 int main(int argc, char const *argv[]) {
 
   int n;
   cout << "enter n?\n";
   cin >> n;
-  solution(int n);
+  solution(n);
 
   return 0;
 }
