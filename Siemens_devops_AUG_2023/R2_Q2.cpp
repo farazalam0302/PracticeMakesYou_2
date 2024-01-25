9a12d43c5970f189c00afd05a0d9e050adc016ed
ALAM, FARAZ <farazalam030@gmail.com>
2024-01-26 02:30:20 +0530

    Add LCSubstring Quiery

--- Siemens_devops_AUG_2023/R2_Q2.cpp
+++ Siemens_devops_AUG_2023/R2_Q2.cpp
@@ -1,14 +1,15 @@
 /*
 [6:06 PM] Palaparthi, Srikrishna (DI SW IES DMA QPE DOPS)
 
-Given two strings ‘X’ and ‘Y’, find the length of the longest common substring.
+Given two strings â€˜Xâ€™ and â€˜Yâ€™, find the length of the longest common
+substring.
 
 [6:07 PM] Palaparthi, Srikrishna (DI SW IES DMA QPE DOPS)
 
-Input : X = “zxabcdezy”, y = “yzabcdezx”
+Input : X = â€œzxabcdezyâ€�, y = â€œyzabcdezxâ€�
 Output : 6
 Explanation:
-The longest common substring is “abcdez” and is of length 6.
+The longest common substring is â€œabcdezâ€� and is of length 6.
 
 LCS of s1 and s2
 eg
@@ -35,12 +36,13 @@ int main(int argc, char const *argv[]) {
 
   vector<vector<int>> dp(x.size() + 1, vector<int>(y.size() + 1, 0));
   dp[0][0] = 1;
-
+  int result = INT_MIN;
   for (int i = 1; i <= x.size(); i++) {
     for (size_t j = 1; j <= y.size(); j++) {
-      if (x[i] == y[j])
+      if (x[i] == y[j]) {
         dp[i][j] = dp[i - 1][j - 1] + 1;
-      else {
+        result = max(result, dp[i][j]);
+      } else {
         dp[i][j] = 0; // max(max(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]);
       }
     }
@@ -55,7 +57,7 @@ int main(int argc, char const *argv[]) {
     cout << endl;
   }
 
-  cout << " Longest Length  =" << dp[x.size()][y.size()];
+  cout << " Longest Length  =" << result;
 
   return 0;
 }
