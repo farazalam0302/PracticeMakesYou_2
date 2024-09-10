a85863a3e05c9da266938731aebb5bbdbc214869
Alam, Faraz <farazalam030@gmail.com>
2024-09-10 14:27:01 +0530

    Linked_Learning_CPP

---
+++ LinkedIn_learning/CPP_Learning_Linkedin/checkForFormat.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <format>
+#include <iostream>
+using namespace std;
+using namespace std;
+using std::cout;
+using std::format;
+
+int main(int argc, char const *argv[]) {
+
+  string s = "12354";
+  int x;
+  stringstream strStream(s);
+  strStream >> x;
+
+  cout << "\nHello World\n"; //<< endl;
+  cout << "\n x+ 1 =" << (x + 1) << endl;
+  cout << "\n str s = " << s << endl;
+
+  return 0;
+}
