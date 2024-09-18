b2cd52ee22f3ccc86d593ad8d5dbb24a7718a17f
Alam, Faraz <farazalam030@gmail.com>
2024-09-18 20:31:16 +0530

    add lambda capture list examples

--- LinkedIn_learning/CPP_Learning_Linkedin/CPP_Advanced_Topic/Lambda_functions/lambda-capture.cpp
+++ LinkedIn_learning/CPP_Learning_Linkedin/CPP_Advanced_Topic/Lambda_functions/lambda-capture.cpp
@@ -3,12 +3,30 @@
 #include <format>
 
 // format-style print()
-constexpr void print(const std::string_view str_fmt, auto&&... args) {
-    fputs(std::vformat(str_fmt, std::make_format_args(args...)).c_str(), stdout);
+constexpr void print(const std::string_view str_fmt, auto &&...args) {
+  fputs(std::vformat(str_fmt, std::make_format_args(args...)).c_str(), stdout);
 }
 
 int main() {
-    const char* greeting {"Hello, earthlings"};
-    auto x = []{ return greeting; };
-    print("{}\n", x());
+  const char *greeting{"Hello"};
+  const char *folks{"Folks"};
+  auto x = [greeting, folks] {
+    return std::format("from x {}, {}", greeting, folks);
+  };
+  print("{}\n", x());
+
+  // capture by refrences
+
+  auto y = [&greeting, &folks] {
+    return std::format("from y {}, {}", greeting, folks);
+  };
+  print("{}\n", y());
+
+  // capture all by refs in scope
+  auto z = [&] { return std::format("from z {}, {}", greeting, folks); };
+  print("{}\n", z());
+
+  // capture all by refs in scope
+  //   auto a = [] { return std::format("from a {}, {}", greeting, folks); };
+  //   print("{}\n", a());
 }
