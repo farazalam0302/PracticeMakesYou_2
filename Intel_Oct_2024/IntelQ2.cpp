dd7bbd4fd11a2f54a5b6813eccc158a84b72a5a0
Alam, Faraz <farazalam030@live.in>
2024-10-22 09:29:33 +0000

    Correct Fact(Long_Digits n) Solution with array

--- Intel_Oct_2024/IntelQ2.cpp
+++ Intel_Oct_2024/IntelQ2.cpp
@@ -1,27 +1,92 @@
-// Compute Fact (N) print ; no float and range 1 <= n <= 100
-// 158 digits  9.332622e+157
+// // Compute Fact (N) print ; no float and range 1 <= n <= 100
+// // 158 digits  9.332622e+157
 #include <bits/stdc++.h>
 using namespace std;
-
-int a[158];
-int factorial(int n) {
-  int lsb = 157;
-  a[lsb] = n;
-  int carry = 0;
-  int tmp = 0;
-  for (int i = n - 1; i >= 1; i--) {
-    tmp = a[lsb] * i + carry;
-    a[lsb - 1] == tmp % 10;
-    carry = tmp / 10;
-    lsb--;
-    // a[lsb-1]=a[lsb]*i/10;ls
+#define DIGITS 500
+void factorial(int n) {
+  int answer[DIGITS]; // = {0};
+  answer[0] = 1;
+  int answer_size = 1;
+  for (int x = 2; x <= n; x++) {
+    int carry = 0;
+    for (int i = 0; i < answer_size; i++) {
+      int prod = answer[i] * x + carry;
+      answer[i] = prod % 10;
+      carry = prod / 10;
+    }
+    while (carry) {
+      answer[answer_size] = carry % 10;
+      carry = carry / 10;
+      answer_size++;
+    }
   }
+  cout << "\nFactorial(" << n << ") = ";
+  for (int i = answer_size - 1; i >= 0; i--) {
+    cout << answer[i];
+  }
+  cout << "\n";
 }
-
 int main(int argc, char const *argv[]) {
-
-  int n = 8;
-  int r = factorial(100);
-  cout << "result = " << r << endl;
+  factorial(100);
+  // cout << "result = " << r << endl;
   return 0;
 }
+// C++ program to compute factorial of big numbers
+#if 0
+#include <iostream>
+using namespace std;
+// Maximum number of digits in output
+#define MAX 500
+// int multiply(int x, int res[], int res_size);
+// This function finds factorial of large numbers
+// and prints them
+void factorial(int n) {
+  int res[MAX];
+  res[0] = 1;
+  int res_size = 1;
+  for (int x = 2; x <= n; x++) {
+    int carry = 0; // Initialize carry
+    for (int i = 0; i < res_size; i++) {
+      int prod = res[i] * x + carry;
+      res[i] = prod % 10;
+      carry = prod / 10;
+    }
+    while (carry) {
+      res[res_size] = carry % 10;
+      carry = carry / 10;
+      res_size++;
+    }
+  }
+  cout << "Factorial of given number is \n";
+  for (int i = res_size - 1; i >= 0; i--)
+    cout << res[i];
+}
+
+// Driver program
+int main() {
+  factorial(100);
+  return 0;
+}
+
+#endif
+
+#if 0
+int multiply(int x, int res[], int res_size) {
+  int carry = 0; // Initialize carry
+  // One by one multiply n with individual digits of res[]
+  for (int i = 0; i < res_size; i++) {
+    int prod = res[i] * x + carry;
+    // Store last digit of 'prod' in res[]
+    res[i] = prod % 10;
+    // Put rest in carry
+    carry = prod / 10;
+  }
+  // Put carry in res and increase result size
+  while (carry) {
+    res[res_size] = carry % 10;
+    carry = carry / 10;
+    res_size++;
+  }
+  return res_size;
+}
+#endif
