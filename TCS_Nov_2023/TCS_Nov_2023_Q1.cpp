68f3564696cc89940cdc39d83f3982ca7db8fc3a
Alam, Faraz <farazalam030@live.in>
2024-11-06 09:27:50 +0000

    Add TCS November 2023 Questions

---
+++ TCS_Nov_2023/TCS_Nov_2023_Q1.cpp
@@ -0,0 +1,36 @@
+// a=b+c
+//
+
+// s=s1 + a; // Polymorphism
+
+/*
+
+class sample{
+int data;
+
+};
+
+*/
+
+Sample &operator+(Sample s1, int a) {}
+
+sample {
+public:
+  int a, int b, int *heap;
+}
+
+A{1, 2, [100]}
+
+sample A;
+sample B = A;
+
+deepCopy
+
+Sample(Sample &A) {
+  a = A.a;
+  b = B.b;
+  heap = new int;
+  heap = *(A.heap);
+}
+
+B { 1, 2, [100] in 1000 }
